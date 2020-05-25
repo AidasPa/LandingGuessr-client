a60ce5e002d5439a41c7d9aa64ffcc3ab5b6b316
Aidas Patas <aidas0330@gmail.com>
2020-05-25 18:40:03 +0300

    more fancy logging

--- index.js
+++ index.js
@@ -4,9 +4,10 @@
   // do a callback function once done bootstrap completes
   bootstrap(function () {
       const logger = require("./services/logger.js");
-      logger.debug("Application bootstraped!");
+      logger.success("Application bootstrapped!");
       const connection = require("./actions/connectToSim.js");
       setInterval(() => {
+          logger.debug("Attempting connection to sim!");
           connection();
       }, 2000);
   });
--- services/logger.js
+++ services/logger.js
@@ -1,18 +1,21 @@
 const colors = require("colors");
 module.exports = {
-    info(msg) {
-        console.log("[info] " + msg);
-    },
-    debug(msg) {
-        console.log(colors.cyan("[debug] " + msg));
-    },
-    error(msg) {
-        console.log(colors.red("[error] " + msg))
-    }, 
-    warn(msg) {
-        console.log(colors.yellow("[warn] " + msg));
-    },
-    event(msg) {
-        console.log(colors.brightMagenta("[event] " + msg))
-    }
-}
+  info(msg) {
+    console.log("[info] " + msg);
+  },
+  debug(msg) {
+    console.log(colors.cyan("[debug] " + msg));
+  },
+  error(msg) {
+    console.log(colors.red("[error] " + msg));
+  },
+  warn(msg) {
+    console.log(colors.yellow("[warn] " + msg));
+  },
+  event(msg) {
+    console.log(colors.brightMagenta("[event] " + msg));
+  },
+  success(msg) {
+    console.log(colors.green("[success] " + msg));
+  },
+};
